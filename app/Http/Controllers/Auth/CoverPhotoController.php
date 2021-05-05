353660eed77e47caf84f9ee975e7f0d92dbee607
Hein-Z <ingyin29122000@gmail.com>
2021-05-05 21:45:52 +0630

    profile image and cover photo

--- app/Http/Controllers/Auth/CoverPhotoController.php
+++ app/Http/Controllers/Auth/CoverPhotoController.php
@@ -3,8 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class CoverPhotoController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'cover_photo' => 'image|max:2048|required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = auth()->user();
+        $cover_photo = $request->cover_photo;
+        $imagePath = Storage::disk('uploads')->put($user->email . '/cover-photo', $cover_photo);
+
+
+        return response()->json(['message' => 'successfully added your cover photo image', 'data' => $user->cover_photo]);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'cover_photo' => 'image|max:2048|required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = auth()->user();
+
+        Storage::disk('uploads')->delete($user->email . '/cover-photo', $user->cover_photo);
+
+        $cover_photo = $request->cover_photo;
+        $imagePath = Storage::disk('uploads')->put($user->email . '/cover-photo', $cover_photo);
+
+
+        $user->update([
+            'cover_photo' => $imagePath
+        ]);
+
+        return response()->json(['message' => 'successfully updated your cover photo', 'data' => $user->cover_photo]);
+    }
 }
--- app/Http/Controllers/Auth/ProfileImageController.php
+++ app/Http/Controllers/Auth/ProfileImageController.php
@@ -13,7 +13,7 @@ class ProfileImageController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'profile_image' => 'image|max:2048',
+            'profile_image' => 'image|max:2048|required',
         ]);
 
         if ($validator->fails()) {
@@ -25,10 +25,31 @@ class ProfileImageController extends Controller
         $imagePath = Storage::disk('uploads')->put($user->email . '/profile-image', $profile_image);
 
 
+        return response()->json(['message' => 'successfully added your image', 'data' => $user->profile_image]);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'profile_image' => 'image|max:2048|required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = auth()->user();
+
+        Storage::disk('uploads')->delete($user->email . '/profile-image', $user->profile_image);
+
+        $profile_image = $request->profile_image;
+        $imagePath = Storage::disk('uploads')->put($user->email . '/profile-image', $profile_image);
+
+
         $user->update([
             'profile_image' => $imagePath
         ]);
 
-        return response()->json($user->profile_image);
+        return response()->json(['message' => 'successfully updated your image', 'data' => $user->profile_image]);
     }
 }
